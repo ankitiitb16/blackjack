7e3239a3788870e23a06cd07adc179722bd3f946
Ankit Ranjan <ankit.ranjan@moneyview.in>
2025-04-22 16:12:24 +0530

    Card class

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Card {
+    Suit suit;
+    int faceValue;
+
+    public Card(Suit suit, int faceValue) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+}
